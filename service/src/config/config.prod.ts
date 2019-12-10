7ffdaa3cd546fb663b23c34b0d8b297e15b1dff7
zuofenghua <11100776@bbktel.com>
2019-12-10 21:34:40 +0800

    test husky

---
+++ service/src/config/config.prod.ts
@@ -0,0 +1,8 @@
+import { EggAppConfig, EggAppInfo, PowerPartial } from 'midway';
+
+export type DefaultConfig = PowerPartial<EggAppConfig>;
+
+export default (appInfo: EggAppInfo) => {
+  const config = {} as DefaultConfig;
+  return config;
+};
