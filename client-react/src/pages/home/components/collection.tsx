79ad60d96a1324a60efaf96d086eb08f7258b670
zuofenghua <11100776@bbktel.com>
2020-01-16 22:16:34 +0800

    feat: dev 18%

--- client-react/src/pages/home/components/collection.tsx
+++ client-react/src/pages/home/components/collection.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { CardActions, Card, CardContent, Typography, CardActionArea } from '@material-ui/core';
-import { ICollection } from '../models/collection';
+import { Card, CardContent, Typography, CardActionArea, Avatar, CardHeader } from '@material-ui/core';
+import { ICollection } from '@/hoxmodel/useCollectionsModel';
+import styles from './collection.css';
 
 type collcetionProps = {
   collection: ICollection,
@@ -9,14 +10,14 @@ type collcetionProps = {
 export default (props: collcetionProps) => {
   const collection = props.collection;
 
+  const avatar = <Avatar src={collection.meta.icon} className={styles.header}/>;
+
   return (
     <Card>
       <CardActionArea>
+        <CardHeader avatar={avatar} title={collection.meta.name} />
         <CardContent>
-          <Typography gutterBottom={true} variant="h5" component="h2">
-            {collection.meta.name}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
+          <Typography variant="body2" color="textSecondary" component="p" noWrap={true}>
             {collection.meta.describe}
           </Typography>
         </CardContent>
